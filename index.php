d306fcbbefb3c9b3945f6170b80e8cd03cab7487
Fabio <fabio_alex1@icloud.com>
2022-02-14 21:56:04 -0300

    Random Number Generator

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Número Randômico entre 1 e 100:  <?php echo rand(1,100); ?></h1>
+
+</body>
+</html>
